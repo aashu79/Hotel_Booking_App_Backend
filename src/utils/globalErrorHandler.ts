46371dc43e843014491b6b80e22e40a320b7de34
aashu79 <aashusah435@gmail.com>
2024-02-09 12:23:26 +0545

    minor change

--- src/utils/globalErrorHandler.ts
+++ src/utils/globalErrorHandler.ts
@@ -4,7 +4,7 @@ import mongoose, { MongooseError } from "mongoose";
 const globalErrorHandler = (error: Error, req: Request, res: Response, next: NextFunction)=>{
     console.error(error);
 if(error instanceof mongoose.Error.ValidationError){
-    const err = error.errors;
+    const err = (error as any).errors;
     const keys = Object.keys(err);
    const errorMessage =  keys.map((key)=>{
         return err[key].message
