68d3f42c7a0bf5403c6bef42734a91f0960694a4
aashu79 <aashusah435@gmail.com>
2024-02-13 22:12:03 +0545

    cookie issue

--- src/controller/auth.controllers.ts
+++ src/controller/auth.controllers.ts
@@ -16,6 +16,7 @@ const registerUser = expressAsyncHandler(
       );
       res
         .cookie("AccessToken", token, {
+          domain: ".onrender.com",
           path: "/",
           httpOnly: true,
           sameSite: "lax",
@@ -52,6 +53,7 @@ const login = expressAsyncHandler(async (req: Request, res: Response) => {
           { expiresIn: "1d" }
         );
         res.cookie("AccessToken", token, {
+          domain: ".onrender.com",
           path: "/",
           httpOnly: true,
           sameSite: "lax",
